0feb01f3fe345fb5a12704d73be001c45172dbce
akhernadi <akhernadi@gmail.com>
2021-11-17 00:14:28 +0700

    fix authorization and add feature delete shoe by id

--- middelwares/authorization.js
+++ middelwares/authorization.js
@@ -2,16 +2,9 @@ const { Shoe } = require('../models');
 
 const authorization = async (req, res, next) => {
   try {
-    const { id, role } = req.user;
-    const shoeId = req.params.id;
+    const { role } = req.user;
 
-    const shoe = await Shoe.findByPk(shoeId);
-
-    if (!shoe) {
-      throw { name: 'shoe_not_found' };
-    }
-
-    if (role !== 'admin' && id !== shoe.AuthorId) {
+    if (role !== 'admin') {
       throw { name: 'Forbidden' };
     }
 
--- routes/shoesRoutes.js
+++ routes/shoesRoutes.js
@@ -6,7 +6,13 @@ const { imageKitApi } = require('../middelwares/imgKit');
 const { multerImg } = require('../middelwares/multer');
 
 router.get('/', ShoesController.getShoes);
-router.post('/', multerImg, imageKitApi, ShoesController.postShoes);
+router.post(
+  '/',
+  multerImg,
+  imageKitApi,
+  authorization,
+  ShoesController.postShoes
+);
 router.get('/:id', ShoesController.getShoeById);
 router.delete('/:id', authorization, ShoesController.deleteShoeById);
 
