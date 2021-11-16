0e8c5469a3ad2163939d871cfc0d70153a9dfce6
abrahamls <abraham.siahaan1997@gmail.com>
2021-11-16 22:42:05 +0700

    endpoint post /login added, required email and password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,45 @@
-const { User } = require('../models')
+const { checkPassword } = require("../helpers/bcrypt");
+const { User } = require("../models");
+const { signToken } = require("../helpers/jwt");
 
-class UserController{
-    static async register(req, res, next) {
-        try {
-            const { name, email, password, rank } = req.body
-            console.log(req.body);
-        } catch (error) {
-            next(error)
-        }
+class UserController {
+  static async register(req, res, next) {
+    try {
+      const { name, email, password, rank } = req.body;
+      const newUser = await User.create({ name, email, password, rank });
+      res.status(201).json({
+        id: newUser.id,
+        name: newUser.name,
+        rank: newUser.rank,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { name: "badRequest", message: "Please enter your email!" };
+      }
+      if (!password) {
+        throw { name: "badRequest", message: "Please enter your password!" };
+      }
+      const foundUser = await User.findOne({ where: { email } });
+      if (!foundUser || !checkPassword(password, foundUser.password)) {
+        throw { name: "unauthorized", message: "invalid email/password" };
+      }
+      const access_token = signToken({
+        id: foundUser.id,
+        name: foundUser.name,
+        email: foundUser.email,
+        rank: foundUser.rank,
+      });
+      res.status(200).json({ access_token });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
+
+module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { errorHandler } = require('../middlewares/errorHandler')
 const router = express()
+const UserController = require('../controllers/UserController')
 
-router.post('/register')
-router.post('/login')
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
 
 router.use(errorHandler)
 
