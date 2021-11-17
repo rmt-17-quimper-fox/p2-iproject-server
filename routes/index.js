258519dfe338f4fd1a2c6dfe8bb07a36fe77a8ec
Giffari17 <giffariyusrul98@mail.ugm.ac.id>
2021-11-17 14:33:39 +0700

    Register Feature is Created Successfully with response correct response status and data

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,50 @@ const userRouter = require('./UserRouter');
 const thirdPartyRouter = require('./ThirdPartyRouter'); 
 const recipeRouter = require('./RecipeRouter');
 const commentRouter = require('./CommentRouter');
+const { getJwtToken, getPayload } = require('../helpers/auth');
+const errorHandler = require('../middlewares/errorHandler');
+const { User } = require('../models');
 
-router.get('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
+    try {
+        let latestId;
+        const latestUser = await User.findOne({ order: [ [ 'createdAt', 'DESC' ] ] });
+        if(!latestUser) {
+            latestId = 1;
+        } else {
+            latestId = latestUser.id;
+        }
+        const { username, email, password } = req.body;
+        const payload = {
+            id: latestId+1,
+            email
+        };
+        const token = getJwtToken(payload);
+        let resp = await User.create({ username, email, password, token});
+        resp = {
+            id: resp.id,
+            email: resp.email,
+        };
+        res.status(201).json(resp);
+    } catch (error) {
+        next(error);
+    }
+})
+router.post('/login', async (req, res, next) => {
     res.send('Hello World!')
 })
-router.get('/login', async (req, res) => {
+router.patch('/forgetpasswords', async (req, res, next) => {
     res.send('Hello World!')
 })
-router.get('/forgetpasswords', async (req, res) => {
+router.patch('/resetpasswords', async (req, res, next) => {
     res.send('Hello World!')
 })
-router.get('/resetpasswords', async (req, res) => {
-    res.send('Hello World!')
-})
-
 
 // router.use()
 router.use(thirdPartyRouter)
 router.use(userRouter)
 router.use(recipeRouter)
 router.use(commentRouter)
+router.use(errorHandler);
 
 module.exports = router;
