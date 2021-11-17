d82ceacc9259502e201178ba2395d0074b3dc1a1
rizkybor <rizkyak994@gmail.com>
2021-11-17 14:55:55 +0700

    input values allow null and validation in models mountain

--- models/mountain.js
+++ models/mountain.js
@@ -11,14 +11,50 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Mountain.belongsToMany(models.User, {through: models.Listing, foreignKey:"MountainId"})
     }
   };
   Mountain.init({
-    nama: DataTypes.STRING,
-    bentuk: DataTypes.STRING,
-    tinggi_meter: DataTypes.STRING,
-    estimasi_letusan_terakhir: DataTypes.STRING,
-    geolokasi: DataTypes.STRING
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg:"Nama not empty" },
+        notNull: { msg:"Nama not null" }
+      }
+    },
+    bentuk: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg:"Bentuk not empty" },
+        notNull: { msg:"Bentuk not null" }
+      }
+    },
+    tinggi_meter: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg:"Tinggi Meter not empty" },
+        notNull: { msg:"Tinggi Meter not null" }
+      }
+    },
+    estimasi_letusan_terakhir: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg:"Estimasi Letusan Terakhir not empty" },
+        notNull: { msg:"Estimasi Letusan Terakhir not null" }
+      }
+    },
+    geolokasi: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg:"Geolokasi not empty" },
+        notNull: { msg:"Geolokasi not null" }
+      }
+    },
   }, {
     sequelize,
     modelName: 'Mountain',
