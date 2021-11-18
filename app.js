31f2cbb0f4c76d6875069858ddc4c829c4c64c55
brorp <ryan.feb28@gmail.com>
2021-11-18 07:31:15 +0700

    deploy 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = proccess.env.PORT || 3000
+const port = process.env.PORT || 3000
 const cors = require("cors")
 const Controller = require('./controllers/controller')
 const authentication = require('./middlewares/authentication')
